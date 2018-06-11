bc73f5c4b2246e86708b86d573846425f9d2495f
kasia13579 <https://github.com/kasia13579/gitrepozytorium.git>
2018-06-11 09:35:32 +0200

    do ogarniecia 2

--- SQL/dlotek.py
+++ SQL/dlotek.py
@@ -5,27 +5,42 @@ import random
 
 
 def main(args):
-    ileliczb = input("podaj ilość typowanych liczb: ")
-    maksliczba = input("Podaj maksymalną losowaną liczbę")
-    print("wytypuj () z () liczb") .format(ileliczb, maksliczba)
+    ileliczb = int(input("Podaj ilość typowanych liczb: "))
+    maksliczba = int(input("Podaj maks. losowaną liczbę: "))
+    print("Wytypuj {} z {} liczb".format(ileliczb, maksliczba))
 
     # losowanie liczb
-    liczby = []  3 lista wylosowanych liczb
-    for i in range(ileliczb):
-        if liczby.conut(liczba) = 0
-            liczby.apppend
-        liczba = random.randint(i, maksliczba +1)  # losowanie liczby <1;10>
-    print("Podałeś:", odp)
 
-       # if liczba == int(odp):  # porównanie odpowiedzi z wylosowaną liczbą
-        #    print("Zgadłeś!")
-         #   break  # przerwanie działania pętli
-        #else:
-         #   print("Spróbuj jeszcze raz!")
+    liczby = []   # lista wylosowanych liczb
+    i = 0
+    # for i in range(ileliczb):
+    while i < ileliczb:
+        liczba = random.randint(1, maksliczba)  # losowanie liczby <1;10>
+        if liczby.count(liczba) == 0:  # sprawdzenie czy wartość jest w liście
+            liczby.append(liczba)
+            i += 1  # powiększ i o 1
+    print(liczby)
 
-    return 3
+    typy = set()  # deklaracja pustego zbioru na typyuzytkownika
+    i = 0
+    while i < ileliczb:
+        typ = input("Podaj liczbę", i + 1)
+        if typ not in typy:  # jeżeli podanego typu nie ma w zbiorze
+            typy.add(typ)
+            i += 1
+
+        # odp = input("Podaj liczbę od 1 do 10: ")
+        # print("Podałeś:", odp)
+
+        # if liczba == int(odp):  # porównanie odpowiedzi z wylosowaną liczbą
+        #     print("Zgadłeś!")
+        #     break  # przerwanie działania pętli
+        # else:
+        #     print("Spróbuj jeszcze raz!")
+
+    return 0
 
 
 if __name__ == '__main__':
     import sys
-sys.exit(main(sys.argv))
+    sys.exit(main(sys.argv))
