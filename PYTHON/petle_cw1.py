d17e0d9f8e2321e30d513a2e967d6d7d4d282054
kl2ag1 <kl2ag1@komp09.lo1cg.org>
2018-09-26 08:42:37 +0200

    pythoncw

--- PYTHON/petle_cw1.py
+++ PYTHON/petle_cw1.py
@@ -6,8 +6,17 @@
 
 
 def main(args):
+    
+    a = int(input("Podaj a: "))
+
+    
+    while a < 75:
+            print()
+ 
+                 
     return 0
 
 if __name__ == '__main__':
     import sys
     sys.exit(main(sys.argv))
+
