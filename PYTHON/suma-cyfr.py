177bfafbd0527d3296123d6147e84673a0784695
kasia13579 <kasiadadada@interia.pl>
2018-10-03 00:32:59 +0200

    schemat suma cyfr

--- PYTHON/suma-cyfr.py
+++ PYTHON/suma-cyfr.py
@@ -5,26 +5,29 @@
 #  DANE WEJŚCIOWE: liczba "l2" co najmniej dwucyfrowa podawana przez użytkownika
 #DANE WYJŚCIOWE: suma cyfr liczby l2 wydrukowana w terminalu
 
-def sumuj_cyfry(l2):
+def suma_cyfr(l2):
+    
     suma = 0
+    
     while l2 > 0:
         suma += l2 % 10
         l2 = int(l2 / 10)
+        
     return suma
 
 def main(args):
-
-    #l2 = int(input("Podaj liczbę 2-cyfrową: "))
-    #while l2 < 10:
-    #    l2 = int(input("Podaj liczbę 2-cyfrową: "))
     
-    assert(sumuj_cyfry(111) ==3)
-    assert(sumuj_cyfry(256) ==13)
-    assert(sumuj_cyfry(12341) ==11)
-    assert(sumuj_cyfry(1245) ==12)
+#    l2 = int(input("Podaj liczbę dwucyfrową: "))
+#    
+#    while l2 < 10:
+#        l2 = int(input("Podaj liczbę dwucyfrową: "))
+    
+    assert(suma_cyfr(438) == 15)
+    assert(suma_cyfr(1181) == 11)
+    assert(suma_cyfr(26) == 8)
     
     return 0
-    
+
 if __name__ == '__main__':
     import sys
     sys.exit(main(sys.argv))
--- PYTHON/suma-cyfr2.py
+++ PYTHON/suma-cyfr2.py
@@ -5,27 +5,29 @@
 #  DANE WEJŚCIOWE: liczba "l2" co najmniej dwucyfrowa podawana przez użytkownika
 #DANE WYJŚCIOWE: suma cyfr liczby l2 wydrukowana w terminalu
 
-def sumuj_cyfry(l2):
+def suma_cyfr(l2):
+    
     suma = 0
-    while l2 > 0:
-        suma += l2 % 10
-        l2 = int(l2 / 10)
+    
+    for cyfra in l2:
+		suma += cyfra
+		        
     return suma
 
 def main(args):
+    
+#    l2 = int(input("Podaj liczbę dwucyfrową: "))
+#    
+#    while l2 < 10:
+#        l2 = int(input("Podaj liczbę dwucyfrową: "))
+    
+	assert(suma_cyfr("113571975") == 39)
+	assert(suma_cyfr("9234566542") == 46)
+	assert(suma_cyfr("12765400002") == 27)
+    
+	return 0
 
-    #l2 = int(input("Podaj liczbę 2-cyfrową: "))
-    #while l2 < 10:
-    #    l2 = int(input("Podaj liczbę 2-cyfrową: "))
-    
-    assert(sumuj_cyfry("111") ==3)
-    assert(sumuj_cyfry("111") ==3)
-    assert(sumuj_cyfry("111") ==3)
-    assert(sumuj_cyfry("111") ==3)
-    
-    
-    return 0
-    
 if __name__ == '__main__':
     import sys
-    sys.exit(main(sys.argv))
+	sys.exit(main(sys.argv))
+
