8ca2a46d124328c5490bc1aadd4d06ff6c5d8ecc
kasia13579 <kasiadadada@interia.pl>
2018-10-16 00:34:40 +0200

    praca domowa euklides

--- PYTHON/NWD_Euklides.py
+++ PYTHON/NWD_Euklides.py
@@ -4,16 +4,26 @@
 #  NWD_Euklides.py
 #  
 
-def nwd_klasyczny
 
-    a = int(input("Podaj 1. liczbę: "))
-    b = int(input("Podaj 2. liczbę: "))
-    
-    while a == b
+def NWD(a, b):
+
+    while a != b:
+        if a > b:
+            a = a - b
+        if a < b:
+            b = b - a
+
+    return a
+
 
 def main(args):
+    a = int(input("Podaj 1. liczbę: "))
+    b = int(input("Podaj 2. liczbę: "))
+    print("Największy wspólny dzielnik {} i {} to {}" .format(a, b, NWD(a, b)))
+
     return 0
+
 
 if __name__ == '__main__':
     import sys
-    sys.exit(main(sys.argv))
+sys.exit(main(sys.argv))
