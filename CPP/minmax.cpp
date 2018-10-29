d3c080e31612c043c5080f3b485ef72a1f3bb8c3
kasia13579 <kasiadadada@interia.pl>
2018-10-29 19:21:06 +0100

    pd minmax

--- CPP/minmax.cpp
+++ CPP/minmax.cpp
@@ -1,38 +1,78 @@
 /*
  * minmax.cpp
+ *
  */
 
+
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
-void wypelnij(int tab[], int roz){
-    cout << "Podaj " << roz << " Liczb: " <<endl;
-    for( int i = 0; i < roz; i++) {
+void wypelnij(int tab[], int roz)
+{   
+    cout << "Podaj " << roz << " liczb: " << endl;
+    
+    for (int i = 0; i < roz; i++)
+    {
         cin >> tab[i];
-            }
+    }
 }
 
-void wypelnij_los(int tab[], int roz){
-    srand(time(NULL));  // inicjacja generatora liczb pseudolosowych
-    for( int i = 0; i < roz; i++) {
-        tab[i] = rand()%101;
-            }
+void wypelnij_losowe(int tab[], int roz)
+{   
+    srand(time(NULL));  // inicjacja generatora pseudolosowych
+    
+    for (int i = 0; i < roz; i++)
+    {
+        tab[i] = rand() % 101;  
+    }
 }
 
-void drukuj(int tab[], int roz){
-    cout << "Podaj " << roz << " Liczb: " <<endl;
-    for( int i = 0; i < roz; i++) {
-        cout << tab[i] << " ";
-            }
+void drukuj(int tab[], int roz)
+{    
+    for (int i = 0; i < roz; i++)
+    {
+        cout << tab[i] << ' ';
+    }
+}
+
+int min1(int tab[], int roz)
+{   
+    int min = tab[0]; 
+    
+    for( int i = 1; i < roz; i++)
+	{
+		if(tab[i] < min)
+			min = tab[i];
+	}
+	
+    return min;    
+}
+
+int max1(int tab[], int roz)
+{   
+    int max = tab[0];
+    
+    for( int i = 1; i < roz; i++)
+	{
+		if(tab[i] > max)
+			max = tab[i];
+	}
+	
+    return max;    
 }
 
 int main(int argc, char **argv)
 {
 	int rozmiar = 50;
-    int tab[rozmiar];  // statyczna deklaracja tablicy
-    wypelnij_los(tab, rozmiar);
-    drukuj(tab, rozmiar);
+    int tablica[rozmiar];
+    
+    wypelnij_losowe(tablica, rozmiar);
+    drukuj(tablica, rozmiar);
+    cout << endl << "Najmniejsza liczba: " << min1(tablica,rozmiar) << endl;
+    cout << "Największa liczba: " << max1(tablica, rozmiar);
+    
 	return 0;
 }
 
--- PYTHON/minmax.py
+++ PYTHON/minmax.py
@@ -4,13 +4,27 @@
 #  minmax.py
 import random
 
+def minmax2(lista):
+    """Funkcja zwraca najmniejszy i największy element z podanej listy"""
+    
+    min = max = lista[0]
+    
+    for liczba in lista:
+        if liczba < min:
+            min = liczba
+        if liczba > max:
+            max = liczba
+            
+    return min, max
+    
 def minmax1():
-    a = int(input('Podaj liczbę: '))
+    
+    a = int(input("Podaj liczbę: "))
     min = a
     max = a
     
     while True:
-        a = int(input('Podaj liczbę: '))
+        a = int(input("Podaj liczbę: "))
         if a < 1:
             break
         if a < min:
@@ -18,32 +32,29 @@ def minmax1():
         if a > max:
             max = a
             
-    return min, max
-    
-def minmax2(lista):
-    min + max = lista.pop(0)
-    min = a
-    max = a
-    
-    for a in lista:
-        if a > min:
-            min = a
-        if a < max:
-            max = a
-    
-    return min, max
+    return min, max    
 
 def main(args):
+    
     # ~min, max = minmax1()
-    # ~print("Najmniejsza liczba:", min)
-    # ~print("Najwieksza liczba:", max)
+    
+    # ~print("Najmniejsza liczba: ", min)
+    # ~print("Największa liczba: ", max)
     
     lista = []
+    
     for i in range(100):
-        lista.append(random.randint(1,100))
+        lista.append(random.randint(1, 100))
+    
     print(lista)
+    
     min, max = minmax2(lista)
+    print("Najmniejsza liczba: ", min)
+    print("Największa liczba: ", max)
+    
+    
+    return 0
 
 if __name__ == '__main__':
     import sys
-    sys.exit(main(sys.argv))
+sys.exit(main(sys.argv))
