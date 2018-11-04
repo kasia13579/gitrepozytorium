1cf5ba943e10bd2303818303b08880d642d752d5
kasia13579 <kasiadadada@interia.pl>
2018-11-04 22:53:47 +0100

    pd anagram

--- CPP/anagram.cpp
+++ CPP/anagram.cpp
@@ -9,25 +9,33 @@
 
 using namespace std;
 
-int zlicz(char tb[]){
-        int i = 0;
-        while(tb[i] != '\0') i++;
-        return i;
-    }
+int zlicz(char tb[]) {
+    int i = 0;
+    while(tb[i] != '\0') i++;
+    return i;
+}
 
-void wyswietl(char tekst[], int roz) {
-        for(int i=k-1;i>=0;i--){
-                cout << tekst[i];
-            }
+void wyswietl(char tekst[], int rozmiar) {
+    for(int i = 0; i < rozmiar; i++) {
+        cout << tekst[i];
     }
+}
 
+void anagram(char tekst[], int rozmiar) {
+    for (int i = rozmiar; i >= 0; i--) {
+        cout << tekst[i];
+    }
+}
 
 int main(int argc, char **argv)
 {
-    const int roz = 50;
-	char tekst[roz];
-    cin.getline(tekst, roz);
+    const int rozmiar = 30;
+    char tekst[rozmiar];
+    cout << "Podaj słowa: ";
+    cin.getline(tekst, rozmiar);
     wyswietl(tekst, zlicz(tekst));
+    cout << "\nSłowa zapisane od tyłu: ";
+    anagram(tekst, zlicz(tekst));
+    
 	return 0;
 }
-
