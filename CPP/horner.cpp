bc91edfb992c22faf795ccce20b20572ccbf11a2
kasia13579 <kasiadadada@interia.pl>
2018-12-08 22:22:09 +0100

    ogarniete2

--- CPP/horner.cpp
+++ CPP/horner.cpp
@@ -4,12 +4,12 @@
  * Copyright 2018  <>
  */
 
-
 #include <iostream>
 
 using namespace std;
 
-void drukujw(int st, float tbwsp[]) {
+void drukujw(int st, float tbwsp[])
+{
     int i = 0;
     for (i = 0; i < st; i++) {
         cout << tbwsp[i] << "x^" << st-i << " + ";
@@ -17,7 +17,15 @@ void drukujw(int st, float tbwsp[]) {
     cout << tbwsp[i] << endl;
 }
 
-float horner_it(int st, float tbwsp[], float x) {
+float horner_re(int st, float tbwsp[], float x)
+{
+    if(st == 0) return tbwsp[0];
+    
+    return x*horner_re(st-1, tbwsp, x) + tbwsp[st];    
+}
+
+float horner_it(int st, float tbwsp[], float x) 
+{
     // 0, 1, 2, 3
     // x (x (2x + 3) + 5) + 4
     float wynik = tbwsp[0];
@@ -27,25 +35,35 @@ float horner_it(int st, float tbwsp[], float x) {
     return wynik;
 }
 
-
 int main(int argc, char **argv)
 {
-    int stopien = 0;
     float x = 0;
+    
+    int stopien = 0;
     cout << "Podaj stopień wielomianu: ";
     cin >> stopien;
-    float *tbwsp;  //wskaźnik - przechowuje adres komórki w pamięci
-    tbwsp = new float [stopien+1];
-    for (int i=0; i <= stopien; i++) {
-        cout << "Podaj współczynnik przy potędze " << stopien-i << ": ";
+    
+    float *tbwsp; //wskaźnik to zmienna, która przechowuje adres komórki w pamięci
+    tbwsp = new float [stopien + 1]; //alokacja tablicy dynamicznej
+    
+    //typ wskaźnika i typ tablicy muszą być takie same
+    
+    for (int i = 0; i <= stopien; i++)
+    {
+        cout << "Podaj współczynnik przy potędze " 
+             << stopien - i << ": ";
         cin >> tbwsp[i];
     }
-    cout << "Podaj argument: ";
+    
+    cout << "Podaj argument x: ";
     cin >> x;
     
-    cout << "Wartość wielomianu o postaci: ";
+    cout << "\nWartość wielomianu o postaci ";
     drukujw(stopien, tbwsp);
-    cout << "\ndla x = " << x << " wynosi: " << horner_it(stopien, tbwsp, x);
+    cout << "dla x = " << x << " wynosi: " 
+         << "\niteracyjnie: " << horner_it(stopien, tbwsp, x)
+         <<"\nrekurencyjnie: " << horner_re(stopien, tbwsp, x);
+         
     cout << endl;
     
 	return 0;
