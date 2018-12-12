de6b421ac672b346ac8fb731cc4f03b1b8c5d78f
kasia13579 <kasiadadada@interia.pl>
2018-12-12 20:56:34 +0100

    sortowanie zrobione

--- CPP/sortowanie.cpp
+++ CPP/sortowanie.cpp
@@ -3,74 +3,96 @@
  */
 
 
+
 #include <iostream>
 
 using namespace std;
 
-void wyswietl(int tab[], int roz) {
-    for(int i = 0; i < roz; i++) {
-        cout<< tab[i] << " ";
+void wypelnij_losowe(int tab[], int roz)
+{   
+    srand(time(NULL));  // inicjacja generatora pseudolosowych
+    
+    for (int i = 0; i < roz; i++)
+    {
+        tab[i] = rand() % 101;  
+    }
+}
+
+void drukuj(int tab[], int roz)
+{    
+    for (int i = 0; i < roz; i++)
+    {
+        cout << tab[i] << ' ';
     }
 }
 
 
-void wypelnij_los(int tab[], int roz) {
-    srand(time(NULL)); // inicjacja generatora liczb pseudolosowych
-    for(int i = 0; i < roz; i++) {
-        tab[i] = rand() % 51;
-    }
-}
-
-void zamien(int &a, int &b) {
+void zamien(int &a, int &b)
+{
     int tmp; //zmienna pomocnicza
     tmp = a;
     a = b;
     b = tmp;
-    //~cout << a << " " << b;
-    // &x oznacza nie kopia a oryginał
 }
 
-void zamien2(int tab[], int &i) {
+void zamien2(int tab[], int i)
+{
     int tmp = tab[i];
     tab[i] = tab[i+1];
     tab[i+1] = tmp;
 }
 
-void wypelnij_sort(int tab[], int roz) {
-    cout << "\nSortowanie bąbelkowe";
+void bubble_sort(int tab[],int n)
+{
+    cout << "\nSortowanie bąbelkowe\n";
     int licznik = 0;
-    for(int j = roz - 1; j > 0; j--) {
-        for(int i = 0; i < j; i++) {
+    for(int j = n - 1; j > 0; j--)
+    {
+        for(int i = 0; i <  j; i++) //pętla wewnętrzna
+        {
             licznik++;
-            while (tab[i] < tab[i+1])
-                zamien(tab[i], tab[i+1]);
+            if(tab[i] > tab[i+1])
+            {
+                //zamiana miejscami
+                zamien2(tab, i);
+            }
         }
     }
-    cout << "liczba powtórzeń: " << licznik << endl;
+    cout << "Powtórzenia: " << licznik << endl;
 }
 
-voidsort_insert(int tab, int n( {
-    cout << "\nSortowanie pprzez wstawienie\n";
+void insert_sort(int tab[], int n)
+{
+    cout << "\nSortowanie przez wstawianie:\n";
+     
     int i, j, tmp;
-    for  ( i = 1; i < n; i++) {
-        tmp = tab[i];
-        j = i -1;
+     
+    for(i = 1; i < n; i++)
+    {
+        tmp = tab[1];
+        j = i - 1;
+        while (j >= 0 && tab[j] > tmp)
+        {
+            tab[j+1] = tab[j];
+            j--;
         }
+        tab[j=1] = tmp;
     }
-
+     
+}
 
 int main(int argc, char **argv)
 {
-    int rozmiar = 40;
-    int tab[rozmiar]; // statyczna deklaracja tablicy
-    wypelnij_los(tab, rozmiar);
-    wyswietl(tab, rozmiar);
+    int rozmiar = 20;
+    int tablica[rozmiar]; //statyczna deklaracja tablicy
+    
+    wypelnij_losowe(tablica, rozmiar);
+    drukuj(tablica, rozmiar);
     cout << endl;
-    wypelnij_sort(tab, rozmiar);
-    wyswietl(tab, rozmiar);
-    //~int a = 10;
-    //~int b = 20;
-    //~zamien(a, b);
-    return 0;
+    bubble_sort(tablica, rozmiar);
+    drukuj(tablica, rozmiar);
+    insert_sort(tablica, rozmiar);
+    drukuj(tablica, rozmiar);
+        
+	return 0;
 }
-
