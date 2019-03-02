5f270fb8801359f64d31e4a2e3b437906c7e0123
kasia13579 <kasiadadada@interia.pl>
2019-03-02 23:27:04 +0100

    cos tam co bylo do uzupelnienia

---
+++ php/skroty.php
@@ -0,0 +1,12 @@
+<?php
+
+    // funkcje skrótu
+    $tekst = 'cokolwiek';
+    echo hash('shal', $tekst);
+    echo '\n';
+    echo hash('shal256', $tekst);
+    echo '\n';
+    $haslo = 'ashjdbhjafvghasdvfdshjb';
+    echo scha;
+
+?>
