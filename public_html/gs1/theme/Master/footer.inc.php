8b91d71eaa98f9c0267106b12bded51ee499b36b
kasia13579 <kasiadadada@interia.pl>
2019-05-24 08:44:54 +0200

    arkusz x2 skonczy GIMPX2 BOOTSTRAP X2

---
+++ public_html/gs1/theme/Master/footer.inc.php
@@ -0,0 +1,5 @@
+<?php if(!defined('IN_GS')){DIE('YOU CANNOT LOAD THIS PAGE DIRECTLY.'); } ?>
+
+<div class="container">
+    
+</div>
---
+++ public_html/gs1/theme/Master/header.inc.php
@@ -0,0 +1,27 @@
+<?php if(!defined('IN_GS')){DIE('YOU CANNOT LOAD THIS PAGE DIRECTLY.'); } ?>
+
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title><?php get_page_clean_title(); ?>&lt; <?php get_site_name(); ?></title>
+
+  <!-- Bootstrap -->
+  <link rel="stylesheet" href="<?php get_theme_url(); ?>css/bootstrap.min.css">
+
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.9/css/all.css" integrity="sha384-5SOiIsAziJl6AWe0HWRKTXlfcSHKmYV4RBF18PPJ173Kzn7jzMyFuTtk8JA7QQG1" crossorigin="anonymous">
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.js"></script>
+  <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+  <![endif]-->
+
+  <!-- twoje style -->
+  <link rel="stylesheet" href="<?php get_theme_url(); ?>css/style.css">
+  <?php get_header(); ?>
+</head>
+
